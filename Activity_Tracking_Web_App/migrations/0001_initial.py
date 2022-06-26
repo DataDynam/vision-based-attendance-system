ca1a19e4dc0f6726d1ac6707ec408ec837c26cb0
DataDynam <159246524+DataDynam@users.noreply.github.com>
2022-06-26 22:27:11 +0000

    Stubs

--- Activity_Tracking_Web_App/migrations/0001_initial.py
+++ Activity_Tracking_Web_App/migrations/0001_initial.py
@@ -3,4 +3,14 @@
 import Activity_Tracking_Web_App.models
 import datetime
 from django.db import migrations, models
-import django.db.models.d
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
