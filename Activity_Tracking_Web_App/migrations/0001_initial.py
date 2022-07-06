6326404737ca7f72b4006b3ca85e0dd9d5e81c3d
DataDynam <159246524+DataDynam@users.noreply.github.com>
2022-07-06 08:42:30 +0000

    new index

--- Activity_Tracking_Web_App/migrations/0001_initial.py
+++ Activity_Tracking_Web_App/migrations/0001_initial.py
@@ -25,4 +25,4 @@ class Migration(migrations.Migration):
             name='Test',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-             
+                ('model_pic', models.ImageField(blank=True, null=True, upload_to=Activity_Tracking_Web_App.models.test_cont
