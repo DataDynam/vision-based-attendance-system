ae31cc4ba72a2363448e42512c0116ab38145183
DataDynam <159246524+DataDynam@users.noreply.github.com>
2022-07-10 18:11:54 +0000

    fix linting issues

--- Activity_Tracking_Web_App/migrations/0001_initial.py
+++ Activity_Tracking_Web_App/migrations/0001_initial.py
@@ -30,4 +30,5 @@ class Migration(migrations.Migration):
         ),
         migrations.CreateModel(
             name='Picture',
-          
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_na
