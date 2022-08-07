36583625cbca4d24f78b1d842ec2260b637743e5
DataDynam <159246524+DataDynam@users.noreply.github.com>
2022-08-07 19:17:57 +0000

    Plural

--- Activity_Tracking_Web_App/models.py
+++ Activity_Tracking_Web_App/models.py
@@ -2,4 +2,9 @@ from django.db import models
 from datetime import datetime
 import os
 
-# Build paths inside the project
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Create your models here.
+
+
+class Employee(models.Model):
+    name = models.Char
