b79c7d85ba09d2c3092618ec4af2e95d588e0045
DataDynam <159246524+DataDynam@users.noreply.github.com>
2022-08-16 12:42:34 +0000

    format tweak

--- Activity_Tracking_Web_App/models.py
+++ Activity_Tracking_Web_App/models.py
@@ -16,4 +16,5 @@ class Employee(models.Model):
 class Activity(models.Model):
     Employee = models.ForeignKey(Employee, on_delete=models.DO_NOTHING)
     date = models.DateField(default=datetime.now)
-    arrival_time = models.Ch
+    arrival_time = models.CharField(max_length=10000, blank=True, null=True)
+    depart_time = models.CharField(max_length=10000, blan
