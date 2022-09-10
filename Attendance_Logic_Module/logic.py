8dc6576ee53ccf6039c57e164fd251a4b16130b4
DataDynam <159246524+DataDynam@users.noreply.github.com>
2022-09-10 14:11:18 +0000

    ALSA: hda - Fix mute-LED fixed mode

---
+++ Attendance_Logic_Module/logic.py
@@ -0,0 +1,18 @@
+
+from clean_data import clean_class
+import pandas as pd
+
+
+for x in range(1, 5):
+  globals()['data%s' % x] = pd.read_csv('test' + str(x) + '.csv', encoding ="latin1")
+# Concatanete them
+df = pd.concat([data1, data2 ,data3, data4])
+df = df.sort_values(['employee_name' , 'timestamp', 'camera_id'])
+df['unified_id'] = df['camera_id']
+cols = df.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+df = df[cols]
+
+df['unified_id'] = df['unified_id'].replace(2, 1)
+df['unified_id'] = df['unified_id'].replace(4, 3)
+####df['unified_id'] = df['unified_id'].replace(6, 5)
