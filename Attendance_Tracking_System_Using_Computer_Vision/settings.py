fa5c451a4f02454cc71d08ef79fba9da891dd481
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-10-27 09:50:33 +0000

    Fixed coding style.

--- Attendance_Tracking_System_Using_Computer_Vision/settings.py
+++ Attendance_Tracking_System_Using_Computer_Vision/settings.py
@@ -19,4 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key us
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'sb1t)*u#*t&n&eu5!v=^8b-7isi#c#^^cjp^-n0nv60au0(h8w'
+
+# SECURITY WARNING: don't run
