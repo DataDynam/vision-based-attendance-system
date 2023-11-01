7f0d13ee87a445723674c508a28a00b97e717876
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-11-01 13:28:04 +0000

    Update!

--- Attendance_Tracking_System_Using_Computer_Vision/settings.py
+++ Attendance_Tracking_System_Using_Computer_Vision/settings.py
@@ -44,4 +44,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.Csr
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middlewa
