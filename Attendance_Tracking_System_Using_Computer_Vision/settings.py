a57087a7a7185e7cfe3a117e7544631190cf5920
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-11-17 06:24:19 +0000

    Update tsconfig.json

--- Attendance_Tracking_System_Using_Computer_Vision/settings.py
+++ Attendance_Tracking_System_Using_Computer_Vision/settings.py
@@ -85,4 +85,6 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_V
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValid
