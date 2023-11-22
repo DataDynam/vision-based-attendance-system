d584d74531a88e5ea7fb76e909b558ebeaea6616
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-11-22 08:19:52 +0000

    Add Default XML

--- Attendance_Tracking_System_Using_Computer_Vision/settings.py
+++ Attendance_Tracking_System_Using_Computer_Vision/settings.py
@@ -110,4 +110,13 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = Tr
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+
+# inst
