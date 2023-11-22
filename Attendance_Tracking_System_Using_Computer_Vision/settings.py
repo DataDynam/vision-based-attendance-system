a0f2b4f4f4afba8366cbd4581270be340d341540
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-11-22 07:03:23 +0000

    Null merge

--- Attendance_Tracking_System_Using_Computer_Vision/settings.py
+++ Attendance_Tracking_System_Using_Computer_Vision/settings.py
@@ -102,4 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://do
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = Tr
