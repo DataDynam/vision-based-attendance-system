865b474ab2885ae5c5615e8f8aed494ddf7a9b95
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-11-24 11:06:26 +0000

    nfsd4: fix xdr4 inclusion of escaped char

--- Attendance_Tracking_System_Using_Computer_Vision/urls.py
+++ Attendance_Tracking_System_Using_Computer_Vision/urls.py
@@ -1,3 +1,6 @@
 """Attendance_Tracking_System_Using_Computer_Vision URL Configuration
 
-The `urlpatterns` list routes URLs to vi
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function 
