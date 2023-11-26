de40411f23caa48f33da9cfee5151e2d749c74dc
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-11-26 20:19:16 +0000

    Release v0.99.0 (#5657)

--- Attendance_Tracking_System_Using_Computer_Vision/urls.py
+++ Attendance_Tracking_System_Using_Computer_Vision/urls.py
@@ -17,4 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings # new
 from django.urls import path, include # new
-from django.con
+from django.conf.urls.static import static # new
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG: # new
