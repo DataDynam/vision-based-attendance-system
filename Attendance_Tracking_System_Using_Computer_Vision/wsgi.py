26b628d971bdb8cac799d57da8bb3f0bb2ea8dc6
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-11-27 21:19:28 +0000

    Add meta description

--- Attendance_Tracking_System_Using_Computer_Vision/wsgi.py
+++ Attendance_Tracking_System_Using_Computer_Vision/wsgi.py
@@ -1,2 +1,6 @@
 """
-WSGI config for Attendance_Tracking_System_Us
+WSGI config for Attendance_Tracking_System_Using_Computer_Vision project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on t
