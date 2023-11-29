db06ddcf3b352fa0262e9402b9cfa3b8ad777c23
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-11-29 09:06:03 +0000

    New tests added

--- Attendance_Tracking_System_Using_Computer_Vision/wsgi.py
+++ Attendance_Tracking_System_Using_Computer_Vision/wsgi.py
@@ -3,4 +3,10 @@ WSGI config for Attendance_Tracking_System_Using_Computer_Vision project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on t
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core
