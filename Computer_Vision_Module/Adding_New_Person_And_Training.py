6f75f80ae0e6f38d937053d9b6688a0f0e7095b3
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-12-10 07:56:18 +0000

    Revert spam

--- Computer_Vision_Module/Adding_New_Person_And_Training.py
+++ Computer_Vision_Module/Adding_New_Person_And_Training.py
@@ -1,4 +1,6 @@
 import numpy as np
 from numpy import load
 from numpy import asarray
-fr
+from numpy import savez_compressed
+from .Variables import EMBEDDINGS_PATH,model,PATH_TO_ADDED_PERSON_FOLDER
+from .Load_Dataset import loa
