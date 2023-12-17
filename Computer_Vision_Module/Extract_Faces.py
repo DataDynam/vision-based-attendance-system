1887a2b81ad02c37fb5437345e292ab94f77ad22
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-12-17 14:15:21 +0000

    install

--- Computer_Vision_Module/Extract_Faces.py
+++ Computer_Vision_Module/Extract_Faces.py
@@ -3,4 +3,9 @@ import mtcnn
 from mtcnn.mtcnn import MTCNN
 from numpy import asarray
 from numpy import load
-from .Variables import det
+from .Variables import detector
+
+
+def extract_face(filename, required_size=(160, 160)):
+  '''
+  THIS FUNCTION TAKES (
