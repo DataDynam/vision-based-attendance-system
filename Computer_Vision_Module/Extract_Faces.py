51409a0593db1bb7d1831f1f66ac241ec86a3a8d
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-12-18 15:52:30 +0000

    improve test coverage

--- Computer_Vision_Module/Extract_Faces.py
+++ Computer_Vision_Module/Extract_Faces.py
@@ -8,4 +8,7 @@ from .Variables import detector
 
 def extract_face(filename, required_size=(160, 160)):
   '''
-  THIS FUNCTION TAKES (
+  THIS FUNCTION TAKES (ONE) IMAGE AS INPUT
+  DETECT AND RETURN (ONE) FACE ARRAY RESIZED --> Using the MTCNN Detector
+  '''
+  image = Image.open(fil
