b4cd07d0fde54e39bc7f7fe8e4df9011b32f0438
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-12-24 18:49:46 +0000

    bump 0.2.0

--- Computer_Vision_Module/Load_Dataset.py
+++ Computer_Vision_Module/Load_Dataset.py
@@ -1,3 +1,8 @@
 from .Load_Faces import load_faces
 from .Variables import EMPLOYEES_NAMES,EMBEDDINGS_PATH
 
+import os
+import numpy as np
+from numpy import savez_compressed
+from numpy import asarray
+from nump
