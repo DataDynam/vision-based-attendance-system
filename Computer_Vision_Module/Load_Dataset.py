530a4f8f993e41a4f9b2fbe262b2d8baceec0d3f
DataDynam <159246524+DataDynam@users.noreply.github.com>
2023-12-25 05:42:59 +0000

    Add TODO list.

--- Computer_Vision_Module/Load_Dataset.py
+++ Computer_Vision_Module/Load_Dataset.py
@@ -9,4 +9,10 @@ from numpy import load
 from numpy import expand_dims
 from numpy import asarray
 from numpy import savez_compressed
-from numpy imp
+from numpy import load
+from os.path import isdir
+from os import listdir
+
+
+
+# load a dataset that contains one subdir for each class that in turn con
