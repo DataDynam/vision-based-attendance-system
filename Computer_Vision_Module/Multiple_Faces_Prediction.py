66047290afeb358e950a7a23183cfe0809ac651d
DataDynam <159246524+DataDynam@users.noreply.github.com>
2024-01-20 14:43:46 +0000

    foundation

--- Computer_Vision_Module/Multiple_Faces_Prediction.py
+++ Computer_Vision_Module/Multiple_Faces_Prediction.py
@@ -1,1 +1,6 @@
-from .Variables import EMBEDDINGS_P
+from .Variables import EMBEDDINGS_PATH, SVM_MODEL_PATH,SVM_CONFIDENCE, model
+from .Get_Embeddings import get_embedding
+import pickle
+import cv2
+import numpy as np
+fr
