2235b0b51e65473470f16e10ca26aed4a8af0b40
DataDynam <159246524+DataDynam@users.noreply.github.com>
2024-01-21 09:46:33 +0000

    trello login

--- Computer_Vision_Module/Multiple_Faces_Prediction.py
+++ Computer_Vision_Module/Multiple_Faces_Prediction.py
@@ -3,4 +3,7 @@ from .Get_Embeddings import get_embedding
 import pickle
 import cv2
 import numpy as np
-fr
+from numpy import asarray
+from numpy import savez_compressed
+from numpy import load
+from numpy import expand_dims
