6bed8bc2dca7a215e58f073c9f1eb11f813728a5
DataDynam <159246524+DataDynam@users.noreply.github.com>
2024-01-23 14:56:08 +0000

    Cleanup package.json

--- Computer_Vision_Module/Multiple_Faces_Prediction.py
+++ Computer_Vision_Module/Multiple_Faces_Prediction.py
@@ -7,3 +7,6 @@ from numpy import asarray
 from numpy import savez_compressed
 from numpy import load
 from numpy import expand_dims
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import 
