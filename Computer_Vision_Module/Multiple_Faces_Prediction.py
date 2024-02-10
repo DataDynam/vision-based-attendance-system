d7fee5b4a89b8b8b39ac36ccc0ebb4b01bb6423b
DataDynam <159246524+DataDynam@users.noreply.github.com>
2024-02-10 21:09:13 +0000

    Update jmeter.md

--- Computer_Vision_Module/Multiple_Faces_Prediction.py
+++ Computer_Vision_Module/Multiple_Faces_Prediction.py
@@ -62,3 +62,7 @@ def multiple_faces(filename, required_size=(160, 160)):
     print(pred_proba[0][prediction[0]])
     if(pred_proba[0][prediction[0]] >= SVM_CONFIDENCE): # SVM Thresholding to Get Known vs UnKnown People was 0.999
       predict_name = out_encoder.inverse_transform(prediction)
+      print("Prediction is",predict_name)
+    else:
+      predict_name = ['UNKNOWN']
+      print("Prediction is",predict_n
