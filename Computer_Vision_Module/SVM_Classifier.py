4295abac7f50b2faf8607b4490a47e9168d99195
DataDynam <159246524+DataDynam@users.noreply.github.com>
2024-02-12 20:23:29 +0000

    Remove unneeded import.

--- Computer_Vision_Module/SVM_Classifier.py
+++ Computer_Vision_Module/SVM_Classifier.py
@@ -1,4 +1,10 @@
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import Normalizer
-from s
+from sklearn.svm import SVC
+import pickle
+import os
+from .Variables import SVM_MODEL_PATH
+
+
+def svm_train(newTrainX, trainy )
