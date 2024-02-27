7a16be356d10ed02b37ab3819a9275274d4d5765
DataDynam <159246524+DataDynam@users.noreply.github.com>
2024-02-27 06:48:37 +0000

    zram: propagate error to user

--- Computer_Vision_Module/Video_Test.py
+++ Computer_Vision_Module/Video_Test.py
@@ -12,4 +12,9 @@ from numpy import savez_compressed
 from numpy import expand_dims
 from numpy import load
 from .Variables import EMBEDDINGS_PATH,detector,model,SVM_MODEL_PATH,PATH_TO_OUTPUT_VIDEOS_DIRECTORY,PATH_TO_OUTPUT_LOGFILE_DIRECTORY,PATH_TO_VIDEO_DIRECTORY,DETECTOR_CONFIDENCE,SVM_CONFIDENCE
-from sklearn.preprocessing i
+from sklearn.preprocessing import LabelEncoder
+from .Get_Embeddings import get_embedding
+
+
+def video_test(video_path, svm_model):
+  name = video_
