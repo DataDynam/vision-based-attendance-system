04ce4e980c457841b7868d33d480be086526c00d
DataDynam <159246524+DataDynam@users.noreply.github.com>
2024-04-29 20:22:46 +0000

    Update working

--- manage.py
+++ manage.py
@@ -9,4 +9,6 @@ def main():
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportErro
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on yo
